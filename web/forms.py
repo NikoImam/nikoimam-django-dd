e869eabbb515005a6b49d6d684a98ec3c0b892e6
NikoImam <niyaz120904@gmail.com>
2024-01-07 09:37:50 +0300

    Added registration and authorization

---
+++ web/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class RegistrationForm(forms.ModelForm):
+    email = forms.EmailField(label='Адрес эл.почты')
+    username = forms.CharField(label='Логин')
+    first_name = forms.CharField(label='Имя')
+    last_name = forms.CharField(label='Фамилия')
+    password = forms.CharField(widget=forms.PasswordInput(), label='Пароль')
+    repeat_password = forms.CharField(widget=forms.PasswordInput(), label='Повторите пароль')
+    field_order = ["first_name", "last_name", "email", "username", "password", "repeat_password"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data['password'] != cleaned_data['repeat_password']:
+            self.add_error('password', 'Пароли не совпадают')
+        return cleaned_data
+
+    class Meta:
+        model = User
+        fields = {"first_name", "last_name", "email", "username", "password", "repeat_password"}
+
+class AuthForm(forms.Form):
+    username = forms.CharField(label='Логин')
+    password = forms.CharField(widget=forms.PasswordInput())
